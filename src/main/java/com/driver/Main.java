831b4c037a9f9556c665715b8e9b38d7e033f150
Pushpak <pushpakmali44@gmail.com>
2023-05-13 04:43:19 +0530

    All changes are done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]){
+    RWOnly first = new RWOnly();
+    first.setName("Pushpak");
+    String name = first.getName();
+  }
 }
